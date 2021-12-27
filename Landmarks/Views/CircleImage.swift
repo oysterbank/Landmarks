361602973f0de48aa103c0fee1e0476eeb666c05
Kris Laratta <krislaratta@gmail.com>
2021-12-27 08:37:20 -0800

    initial commit. tap landmark from list to see details.

---
+++ Landmarks/Views/CircleImage.swift
@@ -0,0 +1,27 @@
+//
+//  CircleImage.swift
+//  Landmarks
+//
+//  Created by Kris Laratta on 12/26/21.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var image: Image
+    
+    var body: some View {
+        image
+            .clipShape(Circle())
+            .overlay {
+                Circle().stroke(.white, lineWidth: 4)
+            }
+            .shadow(radius: 7)
+    }
+}
+
+struct CircleImage_Previews: PreviewProvider {
+    static var previews: some View {
+        CircleImage(image: Image("turtlerock"))
+    }
+}
---
+++ Landmarks/Views/LandmarkList.swift
@@ -0,0 +1,29 @@
+//
+//  LandmarkList.swift
+//  Landmarks
+//
+//  Created by Kris Laratta on 12/26/21.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    var body: some View {
+        NavigationView {
+            List(landmarks) { landmark in
+                NavigationLink {
+                    LandmarkDetail(landmark: landmark)
+                } label: {
+                    LandmarkRow(landmark: landmark)
+                }
+            }
+            .navigationTitle("Landmarks")
+        }
+    }
+}
+
+struct LandmarkList_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkList()
+    }
+}
