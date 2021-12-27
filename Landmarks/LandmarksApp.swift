3f9d726c7863d604b14fd498f70f4b75cb61cc2d
Kris Laratta <krislaratta@gmail.com>
2021-12-26 16:55:23 -0800

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Kris Laratta on 12/26/21.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
